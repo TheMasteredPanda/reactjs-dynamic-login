f894f5c1236c3305700cbd10a3d4593855c30b95
TheMasteredPanda <dukejakemorgan@gmail.com>
2019-01-04 14:37:56 +0000

    Updating repository.

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -3,20 +3,25 @@ import React, { Component } from 'react';
 class LoginComponent extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            incorrect: false
+        }
 
     }
 
     onLoginButtonClick() {
         let passwordInput = document.getElementById("passwordInput");
 
-        if (passwordInput.innerHTML != "test") {
-            passwordInput.classList.add("incorrect-login");
-            // this.forceUpdate();
-            alert("Incorrect password.");
-        } else {
-            alert("Correct password.");
-            return this.props.getPage();
+        if (passwordInput.innerHTML !== "test") {
+            // passwordInput.classList.add("incorrect-login");
+            let copy = this.state;
+            copy.incorrect = true;
+            console.log(`Incorrect boolean: ${copy.incorrect}`);
+            this.setState(copy);
+            return;
         }
+
+        this.props.setPage();
     }
 
     render() { 
@@ -24,7 +29,7 @@ class LoginComponent extends Component {
             <h2 className="title">Username</h2>
             <input type="text" placeholder="Username" className="input"/>
             <h2 className="title">Password</h2>
-            <input id="passwordInput" placeholder="Password" className="input" type="password"/>
+            <input id="passwordInput" placeholder="Password" className={["input", this.state.incorrect && " incorrect-login"].filter(Boolean)} type="password"/>
             {/* <a href="#">Forgot Password?</a> */}
             <button className="button" id="login" onClick={() => {this.onLoginButtonClick()}}>Login</button>
         </div>);
