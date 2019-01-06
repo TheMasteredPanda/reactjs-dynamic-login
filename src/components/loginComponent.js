bb210ec26439908dd008f2adc110ff2ef7fc01c8
TheMasteredPanda <dukejakemorgan@gmail.com>
2019-01-06 14:42:19 +0000

    Finished dynamic login project.

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -4,19 +4,28 @@ class LoginComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            incorrect: false
+            incorrectPassword: false,
+            incorrectUsername: false
         }
 
     }
 
     onLoginButtonClick() {
-        let passwordInput = document.getElementById("passwordInput");
+        let passwordInput = document.getElementById("passwordBox");
+        let usernameInput = document.getElementById("usernameBox");
+        let copy = this.state;
+           
+        if (passwordInput.value !== "test") {
+            console.log(`Incorrect password.`);
+            copy.incorrectPassword = true; 
+        }
 
-        if (passwordInput.innerHTML !== "test") {
-            // passwordInput.classList.add("incorrect-login");
-            let copy = this.state;
-            copy.incorrect = true;
-            console.log(`Incorrect boolean: ${copy.incorrect}`);
+        if (usernameInput.value !== "testUser") {
+            console.log(`Incorrect username.`);
+            copy.incorrectUsername = true;
+        }
+
+        if (copy.incorrectPassword || copy.incorrectPassword || (copy.incorrectPassword && copy.incorrectUsername)) {
             this.setState(copy);
             return;
         }
@@ -24,14 +33,26 @@ class LoginComponent extends Component {
         this.props.setPage();
     }
 
+    onPasswordContentChange() {
+        let copy = this.state;
+        copy.incorrectPassword = false;
+        this.setState(copy);
+    }
+
+    onUsernameContentChange() {
+        let copy = this.state;
+        copy.incorrectUsername = false;
+        this.setState(copy);
+    }
+
     render() { 
         return (<div className="box" id="loginBox">
             <h2 className="title">Username</h2>
-            <input type="text" placeholder="Username" className="input"/>
+            <input id="usernameBox" type="text" placeholder="Username" className={`${this.state.incorrectUsername ? 'input incorrect' : 'input'}`} onChange={() => this.onUsernameContentChange()}/>
             <h2 className="title">Password</h2>
-            <input id="passwordInput" placeholder="Password" className={`input ${this.state.incorrect ? 'incorrect-login' : ''}`} type="password"/>
+            <input id="passwordBox" placeholder="Password" className={`${this.state.incorrectPassword ? 'input incorrect' : 'input'}`} onChange={() => this.onPasswordContentChange()} type="password"/>
             {/* <a href="#">Forgot Password?</a> */}
-            <button className="button" id="login" onClick={() => {this.onLoginButtonClick()}}>Login</button>
+            <button className="button" id="loginButton" onClick={() => {this.onLoginButtonClick()}}>Login</button>
         </div>);
     }
 }
